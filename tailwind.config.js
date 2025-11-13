3c233f0fb82d1c9fc26d0253c71ffbdbed25039a
JoseMCG23 <jocubillogu@est.utn.ac.cr>
2025-11-12 18:56:02 -0600

    Modo oscuro ahora con Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./*.html",
+    "./css/**/*.css",
+    "./js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
